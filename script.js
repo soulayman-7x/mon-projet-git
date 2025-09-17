ff2081aebab1221669a9a2c4bf2dd778aab4f5e1
SOULAYMAN <idbahasevenx@gmail.com>
2025-09-17 13:10:18 +0100

    ajout  de script de boucles

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-let age = 13;
-
-if (age < 18) {
-  console.log("Vous êtes mineur.");
-} else if (age === 18) {
-  console.log("Vous venez d’atteindre la majorité !");
-} else {
-  console.log("Vous êtes majeur.");
-}
-
-
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) continue; // sauter le 5
+    console.log("Nombre : " + i);
+  }
+  
